fc41550df8594deccd98fcf112efc7d09b114dc9
matthieu <matthieujouve@gmail.com>
2011-03-24 19:50:37 +0100

    modif seeds.rb et pgsql pour production

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,29 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
-upcategories = Upcategory.create([{ :name => 'facture', :id => 99 }, { :name => 'pulve' }, { :name => 'labour' }])
+Upcategory.delete_all
+Category.delete_all
+upcategories = Upcategory.create([{ :name => 'facture'}, 
+{ :name => 'pulve' }, 
+{ :name => 'labour' }])
+
+pulve = Upcategory.find_by_name('pulve')
+facture = Upcategory.find_by_name('facture')
+labour = Upcategory.find_by_name('labour')
+
+categories = Category.create([
+{ :name => 'agri', :upcategory => facture}, 
+{ :name => 'maison', :upcategory => facture }, 
+{ :name => 'investissement', :upcategory => facture },
+{ :name => 'fongicide', :upcategory => pulve },
+{ :name => 'herbicide', :upcategory => pulve },
+{ :name => 'glyphosate', :upcategory => pulve },
+{ :name => 'test', :upcategory => pulve },
+{ :name => 'covercrop', :upcategory => labour },
+{ :name => 'vibro', :upcategory => labour },
+{ :name => 'foin', :upcategory => vente },
+{ :name => 'PAC', :upcategory => vente },
+{ :name => 'avoir', :upcategory => vente },
+{ :name => 'essence voiture', :upcategory => diverse },
+{ :name => 'bricolage', :upcategory => diverse },
+{ :name => 'autres', :upcategory => diverse }])
