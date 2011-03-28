613eecb92b3a3fe5fcda6bb73d6fbfadf467c69f
matthieu <matthieujouve@gmail.com>
2011-03-28 08:49:27 +0200

    factype ok, chargement des id en tant que constant dans Factype.rb

--- app/models/factype.rb
+++ app/models/factype.rb
@@ -1,6 +1,11 @@
 class Factype < ActiveRecord::Base
   has_many :factures
 
+#Chargement des id des 3 Factype en tant que constantes:
+  TOTAL = Factype.find_by_name("total").id
+  NULL = Factype.find_by_name("null").id
+  DIFF = Factype.find_by_name("diff").id
+
 #  
 #  atype = Struct.new(:id, :name)
 #
@@ -22,26 +27,9 @@ class Factype < ActiveRecord::Base
 #    return 2
 #  end
 
-
-    
-#  TOTAL =   Factype.find(:first, :conditions => ["name = ?", "total"])
-#  TOTAL_id =Factype.find(:first, :conditions => ["name = ?", "total"]).id
-
-#  NULL =    Factype.find(:first, :conditions => ["name = ?", "null"])
-#  NULL_id = Factype.find(:first, :conditions => ["name = ?", "null"]).id
-
-#  DIFF =    Factype.find(:first, :conditions => ["name = ?", "diff"])
-#  DIFF_id = Factype.find(:first, :conditions => ["name = ?", "diff"]).id
-
 end
-
 
 #SAVE DATA:
 #{ :name => "diff", :display => "diff", :desc => "Les charges associees a cette facture sont affectees a chaques depenses (pulves et labours).\r\nLa somme de toutes les charges associees a la facture est deduit du cout de la facture et le reste (cout_facture - somme_des_charges) est reparti sur les parcelles associees a la facture."}, 
 #{ :name => "total", :display => "cout total", :desc => "Les charges associees a cette facture ne sont pas deduites du cout de la facture.\r\n\r\nIl s'agit simplement de liens virtuels qui permettent d'associer des charges a des facture pour tracer facilement les depenses.\r\n\r\nLe cout de la facture est reparti entre sur les parcelles associees (avec prise en compte du taux associe."}, 
 #{ :name => "diff", :display => "diff", :desc => ""}])
-
-#test mat, a supprimer
-#  TOTAL = Factype.find_by_name("total")
-#  NULL = Factype.find_by_name("null")
-#  DIFF = Factype.find_by_name("diff")
