41c38c6daec1a62592fea4d4ff4281d359f02eaf
matthieu <matthieujouve@gmail.com>
2011-04-14 22:26:28 +0200

    schema and db saves

---
+++ db/schema-save-20110414.rb
@@ -0,0 +1,216 @@
+# This file is auto-generated from the current state of the database. Instead of editing this file, 
+# please use the migrations feature of Active Record to incrementally modify your database, and
+# then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your database schema. If you need
+# to create the application database on another system, you should be using db:schema:load, not running
+# all the migrations from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20100330165713) do
+
+  create_table "categories", :force => true do |t|
+    t.integer  "upcategory_id"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "charges", :id => false, :force => true do |t|
+    t.integer "toto"
+  end
+
+  create_table "facdivs", :force => true do |t|
+    t.integer  "facture_id",  :null => false
+    t.integer  "saison_id",   :null => false
+    t.integer  "category_id"
+    t.integer  "star"
+    t.integer  "user_id"
+    t.string   "name"
+    t.float    "cout"
+    t.string   "ref_client"
+    t.integer  "ref"
+    t.string   "desc"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "factoparcelles", :force => true do |t|
+    t.integer  "parcelle_id"
+    t.integer  "facture_id"
+    t.float    "value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "factures", :force => true do |t|
+    t.integer  "saison_id",     :null => false
+    t.integer  "category_id"
+    t.integer  "factype_id"
+    t.integer  "reportable_id"
+    t.string   "type",          :null => false
+    t.integer  "star"
+    t.integer  "adu"
+    t.date     "date"
+    t.integer  "user_id"
+    t.string   "name"
+    t.float    "cout"
+    t.string   "ref_client"
+    t.integer  "ref"
+    t.text     "desc"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "factypes", :force => true do |t|
+    t.string   "name"
+    t.string   "display"
+    t.text     "desc"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "labours", :force => true do |t|
+    t.integer  "saison_id",       :null => false
+    t.integer  "category_id",     :null => false
+    t.integer  "user_id",         :null => false
+    t.date     "date",            :null => false
+    t.string   "name",            :null => false
+    t.float    "cout_ha_passage"
+    t.text     "desc"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "labtofactures", :force => true do |t|
+    t.integer  "facture_id"
+    t.integer  "labour_id"
+    t.float    "value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "labtoparcelles", :force => true do |t|
+    t.integer  "parcelle_id"
+    t.integer  "labour_id"
+    t.float    "value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "myusers", :force => true do |t|
+    t.string  "login"
+    t.string  "password"
+    t.integer "admin", :limit => 1
+  end
+
+  create_table "parcelles", :force => true do |t|
+    t.integer  "saison_id",      :null => false
+    t.integer  "typeculture_id", :null => false
+    t.string   "name",           :null => false
+    t.float    "surface"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "pulves", :force => true do |t|
+    t.integer  "saison_id",        :null => false
+    t.integer  "category_id",      :null => false
+    t.integer  "user_id",          :null => false
+    t.date     "date",             :null => false
+    t.string   "name",             :null => false
+    t.float    "dosage"
+    t.float    "prix_littre"
+    t.float    "cout_ha_passage"
+    t.float    "cout_fixe"
+    t.text     "desc"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "putofactures", :force => true do |t|
+    t.integer  "facture_id",  :null => false
+    t.integer  "pulve_id",    :null => false
+    t.integer  "value",       :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "putoparcelles", :force => true do |t|
+    t.integer  "parcelle_id", :null => false
+    t.integer  "pulve_id",    :null => false
+    t.float    "value",       :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "saisons", :force => true do |t|
+    t.string "name", :null => false
+  end
+
+  create_table "settings", :force => true do |t|
+    t.integer "saison_id",      :null => false
+    t.integer "value_parcelle", :limit => 1
+    t.integer "detail_desc",    :limit => 1
+    t.integer "detail_ref",     :limit => 1
+  end
+
+  create_table "typecultures", :force => true do |t|
+    t.string "name", :null => false
+  end
+
+  create_table "upcategories", :force => true do |t|
+    t.string   "name",        :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name",        :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "ventes", :force => true do |t|
+    t.integer  "category_id", :null => false
+    t.integer  "saison_id",   :null => false
+    t.float    "prix",       :null => false
+    t.date     "date",        :null => false
+    t.integer  "user_id",     :null => false
+    t.string   "name",        :null => false
+    t.integer  "ref"
+    t.string   "ref_client"
+    t.integer  "star"
+    t.integer  "adu"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "ventoparcelles", :force => true do |t|
+    t.integer "vente_id",    :null => false
+    t.integer "parcelle_id", :null => false
+    t.float   "value",       :null => false
+  end
+
+  create_table "verifs", :force => true do |t|
+    t.string "name"
+  end
+
+  create_table "zones", :force => true do |t|
+    t.string   "name",      :null => false
+    t.float    "surface",   :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "zonetopas", :force => true do |t|
+    t.integer  "parcelle_id",   :null => false
+    t.integer  "zone_id",       :null => false
+    t.float    "value",         :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
---
+++ db/seeds-save-20110414.rb
@@ -0,0 +1,196 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+
+Upcategory.delete_all
+Category.delete_all
+upcategories = Upcategory.create([{ :name => 'facture'}, 
+{ :name => 'pulve' }, 
+{ :name => 'labour' },
+{ :name => 'vente' },
+{ :name => 'diverse' }])
+
+pulve = Upcategory.find_by_name('pulve')
+facture = Upcategory.find_by_name('facture')
+labour = Upcategory.find_by_name('labour')
+vente = Upcategory.find_by_name('vente')
+diverse = Upcategory.find_by_name('diverse')
+
+categories = Category.create([
+{ :name => 'agri', :upcategory => facture}, 
+{ :name => 'maison', :upcategory => facture }, 
+{ :name => 'investissement', :upcategory => facture },
+{ :name => 'fongicide', :upcategory => pulve },
+{ :name => 'herbicide', :upcategory => pulve },
+{ :name => 'glyphosate', :upcategory => pulve },
+{ :name => 'covercrop', :upcategory => labour },
+{ :name => 'vibro', :upcategory => labour },
+{ :name => 'foin', :upcategory => vente },
+{ :name => 'PAC', :upcategory => vente },
+{ :name => 'Avoir', :upcategory => vente },
+{ :name => 'essence voiture', :upcategory => diverse },
+{ :name => 'bricolage', :upcategory => diverse },
+{ :name => 'autres', :upcategory => diverse }])
+
+saison = Saison.create(:name => "Saison-install")
+setting = Setting.create( :saison => saison, :value_parcelle => false, :detail_desc => false, :detail_ref => false)
+
+typecultures = Typeculture.create([
+{:name => "Ble"},
+{:name => "2eme Ble"}, 
+{:name => "Colza"},
+{:name => "RayGrass"},
+{:name => "Jachere"}])
+typeculture = Typeculture.find(:first)
+
+zones = Zone.create([
+{:name => "Mitant", :surface => 5.09},
+{:name => "Bons Pains", :surface => 14.01},
+{:name => "La Rue", :surface => 46.55},
+{:name => "Reserve", :surface => 2.55},
+{:name => "Vallees", :surface => 13.68},
+{:name => "Guiberons", :surface => 3.0},
+{:name => "Taillis", :surface => 19.32},
+{:name => "Brandes", :surface => 2.97},
+{:name => "Feuillets", :surface => 16.05},
+{:name => "Lavernier", :surface => 0.75},
+{:name => "Varennes", :surface => 15.86},
+{:name => "Patureaux", :surface => 8.87},
+{:name => "Prairies", :surface => 13.26},
+{:name => "Le Bourg-1", :surface => 0.52},
+{:name => "Le Bourg-2", :surface => 1.35}])
+
+for zone in zones
+  parcelle = Parcelle.create( :name => zone.name, 
+                              :saison => saison, 
+                              :typeculture => typeculture, 
+                              :surface => zone.surface)
+  zonetopa = Zonetopa.create(:parcelle => parcelle, :zone => zone, :value => zone.surface)
+end
+
+zonetopa = Zonetopa.find(:first)
+parcelle = Parcelle.find(:first)
+zone = Zone.find(:first)
+
+factypes = Factype.create([
+{:name => "diff", :display => "diff", :desc => "test desc"},
+{:name => "total", :display => "total", :desc => "test desc"},
+{:name => "null", :display => "null", :desc => "test desc"}])
+
+users = User.create([
+{:name => "Jouve"},
+{:name => "Trochet"},
+{:name => "Dauger"},
+{:name => "Raffault"},
+{:name => "C le Mobile"},
+{:name => "Credit Agricole Assurances"},
+{:name => "Bastard"},
+{:name => "Daugeron Garage"},
+{:name => "Communaute de communes"},
+{:name => "SIAEP de l'Igneraie"},
+{:name => "France Telecom"},
+{:name => "Dechansiaud"},
+{:name => "Renaud"},
+{:name => "EDF"},
+{:name => "Compta Centre"},
+{:name => "Pneu Europe Service"},
+{:name => "Sodibe"},
+{:name => "Lapeyre"},
+{:name => "Seelec"},
+{:name => "Denormandie Jardinerie"},
+{:name => "MSA"},
+{:name => "Agro-Service"},
+{:name => "CBM"},
+{:name => "Super U"},
+{:name => "DDA"},
+{:name => "PAC"},
+{:name => "Charrier"},
+{:name => "La Poste"},
+{:name => "Brico Depot"},
+{:name => "divers"},
+{:name => "Bricomarche"}
+])
+
+myuser = Myuser.create([
+{:login => "admin",:password => "admin", :admin => true},
+{:login => "matthieu", :admin => false},
+{:login => "elisabeth", :admin => false},
+{:login => "thierry", :admin => false}
+])
+
+#===== TESTING DATA ========
+
+factureA = Debit.create(:name => 'Debit 1', 
+                          :cout => 1000, 
+                          :user => User.find(:first),
+                          :factype => Factype.find_by_name("diff"),
+                          :saison => saison,
+                          :date => '2011-01-01',
+                          :desc => "from seeds",
+                          :category => Category.find_by_name('agri'))
+
+factureB = Debit.create(:name => 'Debit 2', 
+                          :cout => 1000, 
+                          :user => User.find(:first),
+                          :factype => Factype.find_by_name("diff"),
+                          :saison => saison,
+                          :date => '2011-01-01',
+                          :desc => "from seeds",
+                          :category => Category.find_by_name('agri'))
+
+#labourA = Labour.create(  :name => 'Labour 1', 
+#                          :cout_ha_passage => 10, 
+#                          :user => User.find(:first),
+#                          :saison => saison,
+#                          :date => '2011-01-01',
+#                          :desc => "from seeds",
+#                          :category => Category.find_by_name('covercrop'))
+#
+#labourB = Labour.create(  :name => 'Labour 2', 
+#                          :cout_ha_passage => 10, 
+#                          :user => User.find(:first),
+#                          :saison => saison,
+#                          :date => '2011-01-01',
+#                          :desc => "from seeds",
+#                          :category => Category.find_by_name('covercrop'))
+#
+#Labtofacture.create(  :facture_id => factureA.id,
+#                      :labour_id => labourA.id,
+#                      :value => 10)
+#                      
+#Labtofacture.create(  :facture_id => factureB.id,
+#                      :labour_id => labourB.id,
+#                      :value => 11)
+
+pulveA = Pulve.create(  :name => 'Pulve 1', 
+                        :dosage => 0,
+                        :prix_littre => 0,
+                        :cout_ha_passage => 1,
+                        :cout_fixe => 0,
+                        :user => User.find(:first),
+                        :saison => saison,
+                        :date => '2011-01-01',
+                        :desc => "from seeds",
+                        :category => Category.find_by_name('herbicide'))
+
+pulveB = Pulve.create(  :name => 'Pulve 2', 
+                        :dosage => 0,
+                        :prix_littre => 0,
+                        :cout_ha_passage => 0,
+                        :cout_fixe => 10,
+                        :user => User.find(:first),
+                        :saison => saison,
+                        :date => '2011-01-01',
+                        :desc => "from seeds",
+                        :category => Category.find_by_name('glyphosate'))
+
+Putofacture.create(  :facture_id => factureA.id,
+                     :pulve_id => pulveA.id,
+                     :value => 0)
+                      
+Putofacture.create(  :facture_id => factureB.id,
+                     :pulve_id => pulveB.id,
+                     :value => 0)
+                      
+
+                      
