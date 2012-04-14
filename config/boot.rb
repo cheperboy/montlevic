5a40402d3d0e2d3b7c85177923c5180b82823b6f
matthieu <matthieujouve@gmail.com>
2012-04-14 23:57:15 +0200

    added preinitializer.rb

--- config/boot.rb
+++ config/boot.rb
@@ -35,8 +35,21 @@ module Rails
 
   class Boot
     def run
+      # modif matthieu for bundler
+# commented 2 lines
+      # load_initializer
+      # Rails::Initializer.run(:set_load_path)
+# added following:
       load_initializer
-      Rails::Initializer.run(:set_load_path)
+
+       Rails::Initializer.class_eval do
+         def load_gems
+           @bundler_loaded ||= Bundler.require :default, Rails.env
+         end
+       end
+
+       Rails::Initializer.run(:set_load_path)
+      
     end
   end
 
